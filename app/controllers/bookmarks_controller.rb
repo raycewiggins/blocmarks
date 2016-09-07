8c660a474bb32e7cb87a6eceda24b24eebee44d0
raycewiggins <raycewiggins@gmail.com>
2016-09-07 16:01:40 -0500

    User profile added, Navbar fix

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -11,6 +11,7 @@ class BookmarksController < ApplicationController
   def create
     @topic = Topic.find(params[:topic_id])
     @bookmark = @topic.bookmarks.build(bookmark_params)
+    @bookmark.user = current_user
     authorize @bookmark
 
     if @bookmark.save
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  get 'users/show'
 
   root 'welcome#index'
   get 'welcome/about'
