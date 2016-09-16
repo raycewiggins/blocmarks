bc73e9bd3018ec25d3aa684ce27ea2e82e4755da
raycewiggins <raycewiggins@gmail.com>
2016-09-15 20:28:20 -0500

    Various improvements

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,41 +1,35 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+10.times do
+  User.create!(
+    email: Faker::Internet.email,
+    password: Faker::Internet.password
+  )
+end
+users = User.all
 
-#Random Data
-# 10.times do
-#   User.create!(
-#     email: Faker::Internet.email,
-#     password: Faker::Internet.password
-#   )
-# end
-# users = User.all
-#
-# 20.times do
-#   Topics.create!(
-#     title: Faker::Book.title
-#   )
-# end
-# topics = Topic.all
-#
-# 20.times do
-#   Bookmarks.create!(
-#     url: Faker::Book.title
-#   )
-# end
-# bookmarks = Bookmark.all
+20.times do
+  Topic.create!(
+    user: users.sample,
+    title: Faker::Book.title
+  )
+end
+topics = Topic.all
+
+40.times do
+  Bookmark.create!(
+    topic: topics.sample,
+    url: Faker::Book.title
+  )
+end
+bookmarks = Bookmark.all
 
 # Sample User
-# standard = User.create!(
-#   email:    'standard@example.com',
-#   password: 'helloworld'
-# )
+standard = User.create!(
+  email:    'standard@example.com',
+  password: 'helloworld'
+)
 
-#Output
+# Output
 puts 'Seed finished'
+puts "#{User.count} users created"
 puts "#{Topic.count} topics created"
 puts "#{Bookmark.count} bookmarks created"
